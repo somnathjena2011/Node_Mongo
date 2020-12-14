f80036c9d901f798cba4ad0c6bb620d577618bbd
jekobhai <50512475+somnathjena2011@users.noreply.github.com>
2020-12-14 17:02:31 +0530

    Add files via upload

---
+++ models/ninja.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+/*
+"geometry": {
+    "type": "Point",
+    "coordinates": [125.6, 10.1]
+  }
+*/
+
+//schema for geo location
+const GeoSchema = new Schema({
+    type: {
+        type: String,
+        default: 'Point'
+    },
+    coordinates: {
+        type: [Number],
+        index: '2dsphere'
+    }
+});
+
+//create Ninja Schema & model
+const NinjaSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Name field is required']
+    },
+    rank: {
+        type: String
+    },
+    available: {
+        type: Boolean,
+        default: false
+    },
+    //add in geo location
+    geometry: GeoSchema
+});
+
+const Ninja = mongoose.model('ninja',NinjaSchema);
+
+module.exports = Ninja;
